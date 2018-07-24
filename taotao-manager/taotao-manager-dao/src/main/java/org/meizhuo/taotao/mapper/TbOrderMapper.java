6de026770c6584093337b18d6b5533e451ae8189
陈淦 <15007538980@163.com>
2018-07-24 13:32:38 +0800

    使用Mybatis的逆向工程生成对应的mapper和pojo

---
+++ taotao-manager/taotao-manager-dao/src/main/java/org/meizhuo/taotao/mapper/TbOrderMapper.java
@@ -0,0 +1,32 @@
+package org.meizhuo.taotao.mapper;
+
+
+import org.apache.ibatis.annotations.Param;
+import org.meizhuo.taotao.pojo.TbOrder;
+import org.meizhuo.taotao.pojo.TbOrderExample;
+
+import java.util.List;
+
+public interface TbOrderMapper {
+    int countByExample(TbOrderExample example);
+
+    int deleteByExample(TbOrderExample example);
+
+    int deleteByPrimaryKey(String orderId);
+
+    int insert(TbOrder record);
+
+    int insertSelective(TbOrder record);
+
+    List<TbOrder> selectByExample(TbOrderExample example);
+
+    TbOrder selectByPrimaryKey(String orderId);
+
+    int updateByExampleSelective(@Param("record") TbOrder record, @Param("example") TbOrderExample example);
+
+    int updateByExample(@Param("record") TbOrder record, @Param("example") TbOrderExample example);
+
+    int updateByPrimaryKeySelective(TbOrder record);
+
+    int updateByPrimaryKey(TbOrder record);
+}
---
+++ taotao-manager/taotao-manager-dao/src/main/java/org/meizhuo/taotao/mapper/TbUserMapper.java
@@ -0,0 +1,32 @@
+package org.meizhuo.taotao.mapper;
+
+
+import org.apache.ibatis.annotations.Param;
+import org.meizhuo.taotao.pojo.TbUser;
+import org.meizhuo.taotao.pojo.TbUserExample;
+
+import java.util.List;
+
+public interface TbUserMapper {
+    int countByExample(TbUserExample example);
+
+    int deleteByExample(TbUserExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(TbUser record);
+
+    int insertSelective(TbUser record);
+
+    List<TbUser> selectByExample(TbUserExample example);
+
+    TbUser selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") TbUser record, @Param("example") TbUserExample example);
+
+    int updateByExample(@Param("record") TbUser record, @Param("example") TbUserExample example);
+
+    int updateByPrimaryKeySelective(TbUser record);
+
+    int updateByPrimaryKey(TbUser record);
+}
